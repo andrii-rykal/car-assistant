277a37ad7eb5240052b8fb1ea437067bbe5118cb
Andrii Rykal <andreuka79@gmail.com>
2024-12-16 21:14:00 +0200

    test mkcert

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// import mkcert from 'vite-plugin-mkcert';
 import * as path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/car-assistant/',
-  plugins: [react()],
+  plugins: [react()], // , mkcert()
+  // server: {
+  //   port: 3000,
+  //   https: true
+  // },
   resolve: {
     alias: {
       '@styles': path.resolve(__dirname, 'src/assets/styles'),
