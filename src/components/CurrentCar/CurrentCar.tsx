d499fcfd2f5d6d2b876f3d0e3d4c6844abb2cde2
Andrii Rykal <andreuka79@gmail.com>
2024-12-30 13:40:33 +0200

    rename addCar

--- src/components/CurrentCar/CurrentCar.tsx
+++ src/components/CurrentCar/CurrentCar.tsx
@@ -4,11 +4,17 @@ import styles from './CurrentCar.module.scss';
 import { Button } from '../Button';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { AddCarResponse } from '../../types';
-import { fetchCars, setSelectedCar, showingForm } from '../../features/addCarSlice';
+import {
+  fetchCars,
+  setSelectedCar,
+  showingForm,
+} from '../../features/addCarSlice';
 
 export const CurrentCar = () => {
   const dispatch = useAppDispatch();
-  const { cars, currentUserCar, isAddingCar } = useAppSelector(state => state.addCar)
+  const { cars, currentUserCar, isAddingCar } = useAppSelector(
+    state => state.addCar,
+  );
 
   useEffect(() => {
     dispatch(fetchCars());
@@ -16,13 +22,19 @@ export const CurrentCar = () => {
 
   const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
     const selectedId = +e.target.value;
-    const selectedCar = cars.find((car: AddCarResponse) => car.id === selectedId) || null
-    dispatch(setSelectedCar(selectedCar))
-  }
+    const selectedCar =
+      cars.find((car: AddCarResponse) => car.id === selectedId) || null;
+    dispatch(setSelectedCar(selectedCar));
+  };
 
   return (
     <div className={styles.nameCar}>
-      <select name="cars" className={styles.select} defaultValue={'0'} onChange={handleSelectChange}>
+      <select
+        name="cars"
+        className={styles.select}
+        defaultValue={'0'}
+        onChange={handleSelectChange}
+      >
         <option value="0" disabled>
           Choose the car
         </option>
@@ -32,7 +44,9 @@ export const CurrentCar = () => {
           </option>
         ))}
       </select>
-      {currentUserCar && <p>{`${currentUserCar.brand} ${currentUserCar.model} ${currentUserCar.yearOfManufacture}`}</p>}
+      {currentUserCar && (
+        <p>{`${currentUserCar.brand} ${currentUserCar.model} ${currentUserCar.yearOfManufacture}`}</p>
+      )}
       <Button
         text={'Add car'}
         className={styles.addCarBtn}
